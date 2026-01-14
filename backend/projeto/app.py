c2e9c96ddd9df2afb87f2d92e6e9146baaa2a7e1
BrandaoRM01 <raulmb231@gmail.com>
2026-01-13 21:23:08 -0300

    Organizando imports

--- backend/projeto/app.py
+++ backend/projeto/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request
 from config.config import Config
-from extension.extensao import db
-from flask_cors import CORS
+from extension.extensao import db, cors, jwt
+
 
 def init_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     db.init_app(app)
+    jwt.init_app(app)
 
-    CORS(app, 
+    cors(app, 
          supports_credentials=True, 
          resources={r"/*": {
             "origins": ["http://localhost:8080", "http://localhost:8081"]
@@ -17,10 +18,12 @@ def init_app():
     from blueprints.cadastro_bp import cadastro_bp
     from blueprints.login_bp import login_bp
     from blueprints.perfil_bp import perfil_bp
-
+    from blueprints.mensageria_bp import mensageria_bp
+    
     app.register_blueprint(cadastro_bp, url_prefix='/cadastrar')
     app.register_blueprint(login_bp, url_prefix='/login')
     app.register_blueprint(perfil_bp, url_prefix='/perfil')
+    app.register_blueprint(mensageria_bp)
 
     return app
 
